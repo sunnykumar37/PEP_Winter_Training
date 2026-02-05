66960f1f80da5284278315714398dbbaf9a63fe4
sunnykumar37 <sunnyk28912@gmail.com>
2026-02-05 09:34:28 +0530

    Learning use of  shell, register, login and updating thrreadme file

---
+++ Project1/polls/frorms.py
@@ -0,0 +1,7 @@
+from django  import forms
+
+class InputForm(forms.Form):
+    first_name = forms.CharField(max_length=200)
+    last_name =  forms.CharField(max_length=200)
+    roll_number = forms.IntegerField(help_text = "enter6 digit roll mumber")
+    password = forms.CharField(widget = forms.PasswordInput)
